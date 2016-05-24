a46b5601e19edad504a3c7b2bafe2f132f597d05
Vanesa <Vanesa@Vanesa-PC>
2016-05-24 12:55:35 +0300

    welcome page with banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,22 @@
-<html>
-	<head>
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+@extends('app')
+@section('welcome')
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+<link href='{{ URL::asset('styles/welcome.css')}}' rel='stylesheet' type='text/css'>
+ <div class="wrapper" style="height: 200px;overflow: hidden;">
+        <img src="{{ URL::asset('images/welcomeBanner.jpg')}}" alt="" class="welcomeBanner">
+    </div>
+<div class="container-fluid">
+   
+    <div class="row">
+        <div class="col-md-offset-1 col-md-10 col-md-offset-1">
+            <div class="panel panel-default" style="border-radius: 0px;">
+                <div class="panel-body">
+                    <div class="title">Welcome</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Vanessaaaaaaaaaaa</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
-</html>
+@stop
