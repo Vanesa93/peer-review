5e0cab9f27d5d4b2d70b0cf161ab1850f3b7cb9e
Vanesa <Vanesa@Vanesa-PC>
2016-07-25 22:32:30 +0300

    language icon

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#languages">
                     <span class="sr-only">Toggle Navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -12,16 +12,10 @@
                 <a class="navbar-brand" href="#">Peer-Review</a>
             </div>
 
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="collapse navbar-collapse" id="languages">
                 <ul class="nav navbar-nav">
                     <li><a href="/">{{trans('messages.home')}}</a></li>
-                </ul>
-
-                <ul class="nav navbar-nav navbar-right">
-                    @if (Auth::guest())
-                    <li><a href="/auth/login">{{trans('messages.login')}}</a></li>
-                    <li><a href="/auth/register">{{trans('messages.register')}}</a></li>
-                    <li class="dropdown" style="float:right">                        
+                         <li class="dropdown" style="float:right">                        
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style="float:right">
                             <span class="glyphicons glyphicons-globe"></span>
                             <span class="caret"></span>
@@ -35,6 +29,13 @@
 
                         </ul>
                     </li>
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+                    @if (Auth::guest())
+                    <li><a href="/auth/login">{{trans('messages.login')}}</a></li>
+                    <li><a href="/auth/register">{{trans('messages.register')}}</a></li>
+               
                     @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
