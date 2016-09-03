9d6fef936e77f2b9969882f039bfbb5b6efc24e5
Vanesa <Vanesa@Vanesa-PC>
2016-09-03 16:27:53 +0300

    new name - revieIt

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <title>Peer-Review</title>
+        <title>ReviewIt</title>
         <link href="/css/app.css" rel="stylesheet">       
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css">
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
